c94fac8e6ac50f1782773a1b3c38e9cc5b65d2d5
5rahulmangla <5rahulmangla@gmail.com>
2025-04-20 15:50:54 +0530

    few fixes

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -7,9 +7,8 @@ export const Loader = ()=>{
         <div className="loader animate-spin
          w-12 h-12 border-t-4 border-b-4 border-red-500 
          rounded-full">
-            <img className="w-12 h-19 z-10" src="images/logo.png"></img>
          </div>
-      
+         <img className="w-12 h-19 z-10" src="images/logo.png"></img>
         </div>
         </div>
     )
