3661a109050524ea510d957fea2d43f910cfa663
5rahulmangla <5rahulmangla@gmail.com>
2025-04-20 16:10:29 +0530

    ca added

--- src/components/Contract.jsx
+++ src/components/Contract.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contract = () => {
   return (
        <div className='bg-black' >
-         <h1 className='text-1xl font-bold bg-gradient-to-r from-pink-500 to-yellow-500 bg-clip-text text-transparent text-center mb-9 py-2'>CA-67TJj4kdTbCuUSELaaTzXF8VzMMsQX1En8WJKXZLpump</h1>
+         <h1 className='text-xs font-bold bg-gradient-to-r from-pink-500 to-yellow-500 bg-clip-text text-transparent text-center mb-9 py-2'>CA-67TJj4kdTbCuUSELaaTzXF8VzMMsQX1En8WJKXZLpump</h1>
         </div>
   )
 }
