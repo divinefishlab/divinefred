993dbd23f517130441617dee0de4c99526ad0437
5rahulmangla <5rahulmangla@gmail.com>
2025-04-20 16:35:10 +0530

    ca added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,15 +7,15 @@ import xLogo from '/images/xLogo.png';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-2 px-2">
+    <footer className="bg-gray-800 text-white py-2 px-0">
     <div className=" mx-auto text-center flex flex-row justify-between items-center">
     <div>
          <img className='h-10 w-10' src={logo} alt="Logo" />
       </div>
 
       <div>
-      <p className="text-xs py-2">
-        Contract Address-67TJj4kdTbCuUSELaaTzXF8VzMMsQX1En8WJKXZLpump
+      <p className="text-[8px] py-2">
+        CA-67TJj4kdTbCuUSELaaTzXF8VzMMsQX1En8WJKXZLpump
       </p>
       <p className="text-sm ">
         &copy; {new Date().getFullYear()} All Rights Reserved.
@@ -27,7 +27,7 @@ const Footer = () => {
 
       <div className="mt-2">
                      <div className="social-icon-footer ">
-                       <a href="https://pump.fun/coin/67TJj4kdTbCuUSELaaTzXF8VzMMsQX1En8WJKXZLpump"><img src={dexLogo} alt="xChronialsgame" /></a>
+                       <a href="https://pump.fun/coin/A15ZSsm9xEqXB5wvNFiV9YxKBxR5ZG693o4W9WaPpump"><img src={dexLogo} alt="xChronialsgame" /></a>
                        <a href="https://x.com/fredfishgame"><img src={xLogo} alt="tchronialsgame" /></a>
                        <a href="https://t.me/fredfishgame"><img src={teleLogo} alt="" /></a>
                      </div>
--- src/game/UI.jsx
+++ src/game/UI.jsx
@@ -20,7 +20,7 @@ export const UI = () => {
       </h2>
       )}
 
-        <a href="https://pump.fun/coin/67TJj4kdTbCuUSELaaTzXF8VzMMsQX1En8WJKXZLpump">
+        <a href="https://pump.fun/coin/A15ZSsm9xEqXB5wvNFiV9YxKBxR5ZG693o4W9WaPpump">
         <img src="images/DexLogo.png" className="pointer-events-auto absolute top-8 right-20 w-4 h-4 transition duration-300 hover:scale-110 hover:rotate-5 hover:opacity-80" />
         </a>
 
